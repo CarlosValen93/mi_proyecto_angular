0c0d5d49482ab6266683e7dd0c198ba824b67358
Carlos Valenzuela <carlosvalenzuelag93@gmail.com>
2025-02-03 22:38:33 +0100

    intento de select

---
+++ mi_proyecto_angular/src/app/components/search-post/search-post.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Output,  inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PostService } from '../../services/post.service';
+import { ICategory } from '../../interfaces/icategory.interface';
+@Component({
+  selector: 'app-search-post',
+  imports: [FormsModule],
+  templateUrl: './search-post.component.html',
+  styleUrl: './search-post.component.css'
+})
+
+  export class SearchPostComponent  {
+    
+  
+ 
+  }
+
--- mi_proyecto_angular/src/app/pages/home/home.component.ts
+++ mi_proyecto_angular/src/app/pages/home/home.component.ts
@@ -1,22 +1,25 @@
 
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef,OnInit,   Component, inject } from '@angular/core';
 import { PostCardComponent } from "../../components/post-card/post-card.component";
 import { IPost } from '../../interfaces/ipost.interface';
 import { PostService } from '../../services/post.service';
+import { SearchPostComponent } from "../../components/search-post/search-post.component";
 
 @Component({
   selector: 'app-home',
-  imports: [PostCardComponent],
+  imports: [PostCardComponent, SearchPostComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private postService = inject(PostService);
   arrPosts: IPost[] = [];
-  postService = inject(PostService)
+  filteredPosts: IPost[] = [];
 
-  
   ngOnInit() {
-    this.arrPosts = this.postService.getAll()
+    this.arrPosts = this.postService.getAll();
+    this.filteredPosts = [...this.arrPosts];
   }
 
+ 
 }
